771e7ec13288013a1627f8abe4db7db0f988efc5
Dayu Bian <dayub@chinasystems.com>
2023-12-28 14:17:24 +0800

    refactor

--- llm/llm.py
+++ llm/llm.py
@@ -1,7 +1,7 @@
 import logging
 import multiline
 from litellm import completion
-from openai import APIConnectionError, OpenAIError, RateLimitError
+from openai import OpenAIError
 from mako.template import Template
 
 global OPENAI_API_KEY, OPENAI_MODEL, PROMPTS_PATH
@@ -16,22 +16,7 @@ def identify_function(userCommand, functionList):
     logging.debug(prompt_messages)
 
     logging.info("Calling llm API")
-    try:
-        response = completion(
-            model=OPENAI_MODEL,
-            #model="ollama/openchat",
-            messages = prompt_messages,
-            temperature=0,
-            max_tokens=512
-            #api_base="http://localhost:11434"
-        )
-        logging.info(response)
-        response_json = multiline.loads(response.choices[0].message.content.lstrip('```json').rstrip('```'))
-        return response_json
-    except OpenAIError as e:
-        #Handle API error here, e.g. retry or log
-        logging.error(e)
-        return {'code': e.status_code, 'reason': e.message}
+    return call_llm(prompt_messages)
     
 def identify_template(userCommand, templateList):
     logging.info("Generate prompts")
@@ -43,6 +28,9 @@ def identify_template(userCommand, templateList):
     logging.debug(prompt_messages)
 
     logging.info("Calling llm API")
+    return call_llm(prompt_messages)
+    
+def call_llm(prompt_messages):
     try:
         response = completion(
             model=OPENAI_MODEL,
